2952968faa10d33317b9488b54ffe5ccf4a08fac
miquelcanyellas <mcanellasc@esliceu.net>
2021-11-03 10:16:40 +0100

    1

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+document.querySelector(".hamburguer").addEventListener("click", function(){
+	document.querySelector("#main-menu").classList.toggle("active");
+	document.querySelector("#main-menu").classList.toggle("hide");
+});
